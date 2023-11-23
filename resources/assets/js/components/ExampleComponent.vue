c1e40298dce1331ffe4be8e5d41ea7b2f5d49b5c
moneymagoggang <root@DESKTOP-7J055EK>
2023-11-23 22:08:25 +0200

    setup

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,138 @@
+<template>
+
+    <div>
+        <div class="d-flex justify-content-between align-content-center mb-2">
+            <div class="d-flex ">
+                <div >
+                    <div class="d-flex align-items-center ml-4">
+                        <label for="paginate" class="text-nowrap mr-2 mb-0"
+                        >Per Page</label
+                        >
+                        <select v-model="paginate" class="form-control form-control-sm">
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                        </select>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="col-md-4">
+                <input v-model.lazy="search"
+                       type="search"
+                       class="form-control"
+                       placeholder="Search by name,email,phone,or address..."
+                />
+            </div>
+        </div>
+
+
+
+        <div class="card-body table-responsive p-0">
+            <table class="table table-hover">
+                <tbody>
+                <tr>
+                    <th><input type="checkbox"/></th>
+                    <th>
+                        ID
+                    </th>
+                    <th>
+                        Title
+                    </th>
+                    <th>
+                        Slug
+                    </th>
+                    <th>
+                        Content
+                    </th>
+                    <th>
+                        Published
+                    </th>
+                    <th>
+                        Category id
+                    </th>
+                    <th>Created at</th>
+
+                    <th>Action</th>
+                </tr>
+
+                <tr v-for="post in posts.data" :key="post.id">
+                    <td>
+                        <input type="checkbox" />
+                    </td>
+                    <td>{{ post.id }}</td>
+                    <td>{{ post.title }}</td>
+                    <td>{{ post.slug }}</td>
+                    <td>{{ post.content }}</td>
+                    <td>{{ post.published }}</td>
+                    <td>{{ post.category_id }}</td>
+                    <td>{{ post.created_at }}</td>
+                    <td>
+                        <button @click="deletePost(post.id)" class="btn btn-danger btn-sm">
+                            <i class="fa fa-trash" aria-hidden="true"></i>
+                            DELETE
+                        </button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="row mt-4">
+            <div class="col-sm-6 offset-5">
+                <pagination :data="posts" @pagination-change-page="getPosts" :show-disabled="true" :limit="4" size="default"></pagination>
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            posts: {},
+            paginate: 10,
+            search : "",
+        }
+    },
+    watch:{
+        paginate: function(value){
+            this.getPosts();
+        },
+        search: function(value){
+            this.getPosts();
+        },
+
+    },
+    methods: {
+
+        getPosts(page = 1){
+            axios.get('api/posts?page=' + page
+                + '&paginate=' + this.paginate
+                + '&q=' + this.search
+            )
+                .then(response => {
+                    this.posts = response.data;
+                })
+        },
+        deletePost(post){
+            if (confirm("Are you sure you want to delete this post?")) {
+                axios.delete(`/api/posts/${post}`)
+                    .then(() => {
+                        this.getPosts();
+                    })
+                    .catch(error => {
+                        console.error("Error deleting post:", error);
+                    });
+            }
+        }
+    },
+    mounted(){
+        this.getPosts();
+    }
+
+
+}
+</script>
